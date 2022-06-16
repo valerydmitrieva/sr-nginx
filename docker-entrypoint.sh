cd0875387b6ced4c71935b1b1332e94aa4a77fea
Lera Dmitrieva <dmit.valerya@yandex.ru>
2022-06-16 19:20:46 +0300

    fix typo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,6 @@ export BASE_DOMAIN="${BASE_DOMAIN:="example.com"}"
 
 set +x
 
-sed -i -e "s/BASE_URL/${BASE_DOMAIN}/" /etc/nginx/conf.d/default.conf
+sed -i -e "s/BASE_DOMAIN/${BASE_DOMAIN}/" /etc/nginx/conf.d/default.conf
 
 nginx -g 'daemon off;'
